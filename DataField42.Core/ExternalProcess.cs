f048f40aa4306ed9b990bcea2f1089bd43c2cddf
Ahrkylien <henkvgrootheest@gmail.com>
2024-01-02 19:44:28 +0100

    require admin mode when in admin folder

--- DataField42.Core/ExternalProcess.cs
+++ DataField42.Core/ExternalProcess.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 public static class ExternalProcess
 {
-    public static void SwitchTo(string path, string? arguments = null)
+    public static void SwitchTo(string path, string? arguments = null, bool adminMode = false)
     {
         if (arguments == null)
             arguments = string.Empty;
@@ -12,7 +12,11 @@ public static class ExternalProcess
         processStartInfo.UseShellExecute = true;
         //processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
         processStartInfo.WorkingDirectory = Path.GetDirectoryName(path);
-        _ = Process.Start(processStartInfo) ?? throw new Exception("Process recourse can't be started");
+
+        if (adminMode)
+            processStartInfo.Verb = "runas";
+
+        _ = Process.Start(processStartInfo) ?? throw new Exception("Process resource can't be started");
         Environment.Exit(0);
     }
 }
