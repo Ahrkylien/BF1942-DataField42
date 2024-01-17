4bc08efed93dc79ad03b3bf8d141404ca2dc05b7
Ahrkylien <henkvgrootheest@gmail.com>
2024-01-17 16:40:45 +0100

    create updater

---
+++ DataField42 Updater/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace DataField42_Updater;
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+
+    private void ControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+            ToggleWindowState();
+        else
+            DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+    private void MaximizeButton_Click(object sender, RoutedEventArgs e) => ToggleWindowState();
+
+    private void ToggleWindowState() => WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+}
