2bcfe3110238cab195a1fbcc47c948efd4eabce1
Ahrkylien <henkvgrootheest@gmail.com>
2026-06-13 11:44:52 +0200

    Add patch assembly to source

---
+++ Client Patches/hook function missing map.asm
@@ -0,0 +1,88 @@
+bits 32
+org 0x40BF10
+
+ergc equ 0x8C52D0
+
+patch_start:
+    lea     ecx, [esp-0x94]             ; part I replaced at the caller
+
+    push    ebp
+    push    eax
+    push    ebx
+    push    ecx
+    push    edx
+    push    edi
+    push    esi
+    mov     ecx, ebp                    ; old bp
+    mov     ebp, esp
+    sub     esp, 0x300                  ; room for 128 variables 0x4 - 0x200
+
+    push    0                           ; NULL
+
+    mov     eax, [0x971eac]             ; dice::bf::setup
+    lea     ecx, [eax+0x7D4]            ; std::basic_string dice::bf::setup->modid
+    call    [0x8c30dc]                  ; c_str()
+    push    eax                         ; *arg6
+
+    lea     eax, [esi+0x0C]             ; map path
+    push    eax                         ; *arg5
+
+    lea     ecx, [edi+0x2D4]            ; std::basic_string password
+    call    [0x8c30dc]                  ; c_str()
+    mov     ecx, eax
+
+    ; add quotes around password:
+    xor     ebx, ebx
+    mov     byte [ebp-0x200], '"'
+.replace_loop_0:
+    mov     al, [ecx+ebx]
+    inc     ebx
+    mov     [ebp+ebx-0x200], al
+    test    al, al
+    jnz     short .replace_loop_0
+    mov     byte [ebp+ebx-0x200], '"'
+    mov     byte [ebp+ebx-0x1FF], 0
+    ; push local string address to stack:
+    lea     eax, [ebp-0x200]
+    push    eax                         ; *arg4 (password)
+
+    lea     ecx, [edi+0x22C]            ; std::basic_string IP:port
+    call    [0x8c30dc]                  ; c_str()
+    push    eax                         ; *arg3
+
+    ; add quotes around register path:
+    xor     ebx, ebx
+    mov     byte [ebp-0x100], '"'
+.replace_loop:
+    mov     al, [ergc+ebx]
+    inc     ebx
+    mov     [ebp+ebx-0x100], al
+    test    al, al
+    jnz     short .replace_loop
+    mov     byte [ebp+ebx-0x100], '"'
+    mov     byte [ebp+ebx-0xFF], 0
+    ; push local string address to stack:
+    lea     eax, [ebp-0x100]
+    push    eax                         ; *arg2 (register path for ergc)
+
+    push    map_identifier              ; *arg1
+    push    exeName                     ; *arg0
+    push    exeName                     ; *cmdname
+    push    2                           ; mode
+    call    [0x8c34f8]                  ; _spawnl
+    add     esp, 0x14                   ; 4*times of push
+
+    mov     esp, ebp                    ; restore ESP
+    pop     esi
+    pop     edi
+    pop     edx
+    pop     ecx
+    pop     ebx
+    pop     eax
+    pop     ebp                         ; restore caller's EBP
+    ret                                 ; pop the return address into EIP
+
+exeName:
+    db "DataField42", 0x2E, "exe", 0
+map_identifier:
+    db "map", 0
